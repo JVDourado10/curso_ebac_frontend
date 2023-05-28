5d472f9786836a42224380093ce65107fa25d043
JVDourado10 <128549041+JVDourado10@users.noreply.github.com>
2023-05-28 13:20:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  let tarefas = "";
+  const tarefasArray = [];
+
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+
+    const novaTarefa = $("input").val();
+    const addTarefa = `<li>${novaTarefa}</li>`;
+
+    if (tarefasArray.includes(novaTarefa)) {
+      alert("Você já adicinou essa tarefa");
+    } else {
+      $(addTarefa).appendTo(".tarefas ul");
+      tarefasArray.push(novaTarefa);
+    }
+
+    $("input").val("");
+  });
+
+  $("li").click(function () {
+    console.log($(this).css("text-decoration"));
+    if ($(this).css("text-decoration") == "none solid rgb(0, 0, 0)") {
+      $(this).css("text-decoration", "line-through");
+      console.log($(this).css("text-decoration"));
+    } else {
+      $(this).css("text-decoration", "none");
+      console.log($(this).css("text-decoration"));
+    }
+  });
+});
