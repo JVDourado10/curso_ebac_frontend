aff4f4afb602cb63bbc19d83ea3abecf912fa1f0
João Victor Dourado <128549041+JVDourado10@users.noreply.github.com>
2023-07-20 10:48:20 -0300

    commit inicial

---
+++ main.ts
@@ -0,0 +1,7 @@
+const multiplica = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplica(10, 2)) ;
+
+const sauda = (nome: string): void => console.log(`Olá, ${nome}`);
+
+sauda('João');
